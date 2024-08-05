0dbaa40625ac07bb48c3dc549b4fb9e9ddd89aa4
Ddiegodev <ddiegodev@gmail.com>
2024-08-05 10:50:39 -0500

    Deploy test

--- components/shared/product/product-list.tsx
+++ components/shared/product/product-list.tsx
@@ -1,12 +1,16 @@
-import ProductCard from './product-card'
+import React from 'react'
 
-const ProductList = ({ data }: { data: any }) => {
+import ProductCard from './product-card'
+import { Product } from '@/types'
+
+const ProductList = ({ title, data }: { title: string; data: Product[] }) => {
   return (
-    <>
+    <section className="my-10">
+      <h2 className="h2-bold mb-4">{title}</h2>
       {data.length > 0 ? (
         <div>
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-            {data.map((product: any) => (
+            {data.map((product: Product) => (
               <ProductCard key={product.slug} product={product} />
             ))}
           </div>
@@ -16,7 +20,7 @@ const ProductList = ({ data }: { data: any }) => {
           <p>No product found</p>
         </div>
       )}
-    </>
+    </section>
   )
 }
 
